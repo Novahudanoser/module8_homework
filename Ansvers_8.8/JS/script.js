960392baf79f88848155e3320c4de8cfd44d2db8
Novahudanoser <alex.ramza@gmail.com>
2025-06-15 19:47:50 +0300

    Anvers_8.8

---
+++ Ansvers_8.8/JS/script.js
@@ -0,0 +1,41 @@
+const button = document.querySelector(".btn-gallery");
+const grid = document.querySelector(".gallery-grid");
+const loader = document.querySelector(".loader");
+
+async function loadImages() {
+    try {
+        loader.style.display = "block";
+        button.style.display = "none";
+        grid.innerHTML = "";
+
+        for (let i = 0; i < 20; i++) {
+            const response = await fetch(
+                "https://dog.ceo/api/breeds/image/random"
+            );
+            const data = await response.json();
+
+            const img = document.createElement("img");
+            img.classList.add("gallery-image");
+            img.src = data.message;
+
+            grid.appendChild(img);
+        }
+    } catch (error) {
+        console.error("Ошибка при загрузке изображений! Установите и запуститеVPN!:", error);
+        alert("Произошла ошибка при загрузке изображений! Установите и запустите VPN!");
+    } finally {
+        loader.style.display = "none";
+        button.style.display = "block";
+    }
+}
+
+button.addEventListener("click", loadImages);
+
+document.getElementById("vpnButton").addEventListener("click", (e) => {
+    e.stopPropagation();
+    window.open(
+        "https://adguard-vpn.com/ru/welcome.html",
+        "_blank",
+        "noopener noreferrer"
+    );
+});

601d0160ab83e59c5ae3553bfff392f0e5046496
Novahudanoser <alex.ramza@gmail.com>
2025-06-16 11:41:23 +0300

    Исправлена загрузка картинок API

--- Ansvers_8.8/JS/script.js
+++ Ansvers_8.8/JS/script.js
@@ -8,21 +8,25 @@ async function loadImages() {
         button.style.display = "none";
         grid.innerHTML = "";
 
-        for (let i = 0; i < 20; i++) {
-            const response = await fetch(
-                "https://dog.ceo/api/breeds/image/random"
-            );
-            const data = await response.json();
+        const response = await fetch(
+            "https://dog.ceo/api/breeds/image/random/20"
+        );
+        const data = await response.json();
 
+        data.message.forEach((url) => {
             const img = document.createElement("img");
             img.classList.add("gallery-image");
-            img.src = data.message;
-
+            img.src = url;
             grid.appendChild(img);
-        }
+        });
     } catch (error) {
-        console.error("Ошибка при загрузке изображений! Установите и запуститеVPN!:", error);
-        alert("Произошла ошибка при загрузке изображений! Установите и запустите VPN!");
+        console.error(
+            "Ошибка при загрузке изображений! Установите и запуститеVPN!:",
+            error
+        );
+        alert(
+            "Произошла ошибка при загрузке изображений! Установите и запустите VPN!"
+        );
     } finally {
         loader.style.display = "none";
         button.style.display = "block";
